0802a402072a80e175ec092b566a1532d35fc4a4
KeepGoingPawn <15321275650@163.com>
2019-05-05 16:22:28 +0800

    add test class

---
+++ foundation/test/MyTest.java
@@ -0,0 +1,21 @@
+import datastructure.impl.ArrayStack;
+import org.junit.Test;
+
+/**
+ * @Author:Kevin
+ * @Description:
+ * @Date: 2019/5/5 16:12
+ */
+public class MyTest {
+    @Test
+    public void test() {
+        ArrayStack as = new ArrayStack(4);
+        int[] src = {5,8,9,7};
+        for(int i = 0; i< src.length; i++){
+            as.push(src[i]);
+        }
+        for(int i = 0; i<src.length;i++){
+            System.out.print(as.pop());
+        }
+    }
+}
