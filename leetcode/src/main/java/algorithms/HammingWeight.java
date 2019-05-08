3d99242072313c6fa5260ecc3599b6b5a211c78b
KeepGoingPawn <15321275650@163.com>
2019-05-08 08:54:24 +0800

    add Number of 1 Bits 计算无符号整形数中有多少个为1的位 eg:1111 out:4
    eg:100001
    out:2

---
+++ leetcode/src/main/java/algorithms/HammingWeight.java
@@ -0,0 +1,28 @@
+package algorithms;
+
+/**
+ * @Author:Kevin
+ * @Description:
+ * @Date: 2019/5/8 8:40
+ */
+public class HammingWeight {
+    public static int hammingWeight(int n) {
+        int count = 0;
+        for(int i = 0; i < 32; i++){
+            if((n & 1) == 1){
+                count++;
+            }
+            n = n>>>1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String str = "1111111111111111111111111111111";
+        int n = Integer.parseInt(str,2);
+        System.out.println(n);
+        System.out.println(Integer.MAX_VALUE);
+        int count = hammingWeight(n);
+        System.out.println(count);
+    }
+}
